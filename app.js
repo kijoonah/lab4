c6f6b81d03620bfb9001866512cae6226e8d34f1
Paul Kim <pkk001@ucsd.edu>
2020-01-30 10:28:08 -0800

    node.js lab

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+var index = require("./routes/index");
+var project = require("./routes/project");
 
 var index = require('./routes/index');
 var hello = require('./routes/hello');
@@ -38,6 +40,8 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
+app.get("/project/:name",project.viewProject);
+
 // Example route
 // app.get('/users', user.list);
 
